6d4775f1c553b7cf90b15ed70c948913a035b0c4
Bullets-jyr <jyr@bullets.co.kr>
2023-01-08 10:58:39 +0900

    06. 단어장 기능 구현하기 (2) - 읽기, 삭제

--- app/src/main/java/kr/co/bullets/chapter7/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/chapter7/MainActivity.kt
@@ -1,9 +1,11 @@
 package kr.co.bullets.chapter7
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import kr.co.bullets.chapter7.databinding.ActivityMainBinding
@@ -11,6 +13,14 @@ import kr.co.bullets.chapter7.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity(), WordAdapter.ItemClickListener {
     private lateinit var binding: ActivityMainBinding
     private lateinit var wordAdapter: WordAdapter
+    private var selectedWord: Word? = null
+    private val updateAddWordResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        val isUpdated = result.data?.getBooleanExtra("isUpdated", false) ?: false
+
+        if (result.resultCode == RESULT_OK && isUpdated) {
+            updateAddWord()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,8 +30,13 @@ class MainActivity : AppCompatActivity(), WordAdapter.ItemClickListener {
         initRecyclerView()
         binding.addButton.setOnClickListener {
             Intent(this, AddActivity::class.java).let {
-                startActivity(it)
+//                startActivity(it)
+                updateAddWordResult.launch(it)
             }
+        }
+
+        binding.deleteImageView.setOnClickListener {
+            delete()
         }
     }
 
@@ -40,9 +55,50 @@ class MainActivity : AppCompatActivity(), WordAdapter.ItemClickListener {
             val dividerItemDecoration = DividerItemDecoration(applicationContext, LinearLayoutManager.VERTICAL)
             addItemDecoration(dividerItemDecoration)
         }
+
+        Thread {
+            val list = AppDatabase.getInstance(this)?.wordDao()?.getAll() ?: emptyList()
+//            Thread.sleep(1000)
+            wordAdapter.list.addAll(list)
+            runOnUiThread {
+                wordAdapter.notifyDataSetChanged()
+            }
+        }.start()
+    }
+
+    private fun updateAddWord() {
+        Thread {
+            AppDatabase.getInstance(this)?.wordDao()?.getLatestWord()?.let { word ->
+                wordAdapter.list.add(0, word)
+                runOnUiThread {
+                    wordAdapter.notifyDataSetChanged()
+                }
+            }
+        }.start()
+    }
+
+    private fun delete() {
+        if (selectedWord == null) return
+
+        Thread {
+            selectedWord?.let { word ->
+                AppDatabase.getInstance(this)?.wordDao()?.delete(word)
+                runOnUiThread {
+                    wordAdapter.list.remove(word)
+                    wordAdapter.notifyDataSetChanged()
+                    binding.textTextView.text = ""
+                    binding.meanTextView.text = ""
+                    Toast.makeText(this, "삭제가 완료됐습니다", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }.start()
     }
 
     override fun onClick(word: Word) {
+        selectedWord = word
+        binding.textTextView.text = word.text
+        binding.meanTextView.text = word.mean
+
         Toast.makeText(this, "${word.text} 가 클릭 됐습니다", Toast.LENGTH_SHORT).show()
     }
 }
